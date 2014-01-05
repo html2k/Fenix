7e464059045e01d7931545af8928b537cb349883
LegGnom[Mac] <leggnom@gmail.com>
2014-01-06 02:36:41 +0400

    fix

--- ext/extentions.php
+++ ext/extentions.php
@@ -1682,4 +1682,464 @@
 		),
 		'path' => 'ya-metrika',
 	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
+	); ?><? return $extensionManifest = array(
+	'ya-metrika' => array(
+		'name' => 'Яндекс Метрика',
+		'ico' => '',
+		'index' => 'index.php',
+		'action' => array(
+			'home.widget' => 'widget.php',
+		),
+		'path' => 'ya-metrika',
+	)
 	); ?>
